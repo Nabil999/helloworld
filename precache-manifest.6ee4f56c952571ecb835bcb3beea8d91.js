fe8066de9e90d3f2ce5b9d27884e6d2d64fa206a
nabil999 <nabil.farouk@gmail.com>
2019-12-13 03:11:00 +0200

    Updates

---
+++ precache-manifest.6ee4f56c952571ecb835bcb3beea8d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32efc776035bcf6ad5c0d13826e387c",
+    "url": "/helloworld/index.html"
+  },
+  {
+    "revision": "38a259ab13f5718503a6",
+    "url": "/helloworld/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ea25a4b19db611e57a76",
+    "url": "/helloworld/static/js/2.1dece674.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/helloworld/static/js/2.1dece674.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38a259ab13f5718503a6",
+    "url": "/helloworld/static/js/main.eb7e3885.chunk.js"
+  },
+  {
+    "revision": "36940984062da5f393f1",
+    "url": "/helloworld/static/js/runtime-main.ad259aad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloworld/static/media/logo.5d5d9eef.svg"
+  }
+]);
